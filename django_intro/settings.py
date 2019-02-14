7cbb5133174b55579d76df965be41c40e347e973
woonji913 <johnnyboy0913@gmail.com>
2019-02-14 05:37:05 +0000

    11_Template inheritance_2

--- django_intro/settings.py
+++ django_intro/settings.py
@@ -57,8 +57,10 @@ ROOT_URLCONF = 'django_intro.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'django_intro', 'templates')],
+        # PROJECT01/django_intro/templates/
         'APP_DIRS': True,
+        # INSTALLED APP에 설정된 APP의 디렉토리에 있는 templates를 템플릿으로 활용한다.
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
